636ce2a742302e8f9b590ec4767b425482e9f251
Sergei Budygin <145099561+Svbudygin@users.noreply.github.com>
2024-05-18 18:54:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import telebot
 import time
 TOKEN = '6368959206:AAEQ4hz1iH0IvtpwseWmsmLGF588tY2sZRM'  # Замените на токен вашего бота
-CHANNEL_ID = '-1002030765344'  # Замените на ID вашего канала
+CHANNEL_ID = "-1002001164153"
+# '-1002030765344'  # Замените на ID вашего канала
 
 bot = telebot.TeleBot(TOKEN)
 
-def send_message_error_to_channel():
-    bot.send_message(CHANNEL_ID, "ГГ кажется бот помер")
+def send_message_error_to_channel(url):
+    bot.send_message(CHANNEL_ID, f"ГГ кажется бот помер\n {url}")
+
+
 def send_message_to_channel(data):
-    message = f"""🎮 <b>{data["name"]}</b> 🎮
+    
+    message = f"""🧨 { 'Counter-Strike' if data["is_cs"] else 'Dota'} 🧨
+🎮 {data["name"]} 🎮
 
 ‼️ <b>{data["team_1"]}</b> - Коэффициент победы: <b>{data["coef_win_1"]}</b>
 ‼️ <b>{data["team_2"]}</b> - Коэффициент победы: <b>{data["coef_win_2"]}</b>
 
+⏱ {data["datee"]} {data["timee"]} ⏱
+
 💰🤑 Поставьте свои ставки сейчас! 🤑💰"""
-
+    time.sleep(3)
     bot.send_message(CHANNEL_ID, message, parse_mode="html")
-    time.sleep(1)
-
+    
